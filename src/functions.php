91361bdeb7bdf2202b7ea208da51a5ab960b670e
Andrew Rhyand <andrew.rhyand@gmail.com>
2020-05-04 18:58:37 -0700

    fix formatting

--- src/functions.php
+++ src/functions.php
@@ -24,11 +24,13 @@ if (!function_exists('getallheaders')) {
     function getallheaders()
     {
         $headers = [];
+
         foreach ($_SERVER as $name => $value) {
             if (substr($name, 0, 5) == 'HTTP_') {
                 $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
             }
         }
+
         return $headers;
     }
 }
