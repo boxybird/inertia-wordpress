044a85ee99ec663b32e1f64a686bd1fa2c03889b
Benjamin Niess <benjamin@web-id.fr>
2023-03-23 09:34:27 +0100

    :package: Release 1.0.0

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '5d0e8c61a89171dd32dcc47dfdac83f4f50fe9e2',
+        'reference' => '2b612548b5eea4af97cdf3822d1142b319cb7a7f',
         'name' => 'web-id-fr/inertia-wordpress',
         'dev' => true,
     ),
     'versions' => array(
         'web-id-fr/inertia-wordpress' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '5d0e8c61a89171dd32dcc47dfdac83f4f50fe9e2',
+            'reference' => '2b612548b5eea4af97cdf3822d1142b319cb7a7f',
             'dev_requirement' => false,
         ),
     ),
